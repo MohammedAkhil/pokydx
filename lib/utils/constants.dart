b5b346a3b7c9c2777c5030ffb127204abf71a055
Mohammed Akhil <akhilmohammed05@gmail.com>
2019-03-06 11:43:32 +0530

    add constants and types

---
+++ lib/utils/constants.dart
@@ -0,0 +1,7 @@
+String baseHappinessDescription = """
+    A Pokémon's base friendship is the value to which its friendship
+     is set when the Pokémon is caught, received from an event or NPC, or
+      received from a trade with another player. If a Pokémon is caught in a Friend Ball,
+       however, it will always start out with 200 friendship. If a Pokémon hatched from an Egg,
+        it will start with 120 friendship.
+    """;
