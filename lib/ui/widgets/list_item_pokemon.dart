6f952be4cdf9fe40b028eb824ec1da5334d02b29
Mohammed Akhil <akhilmohammed05@gmail.com>
2019-03-06 11:46:12 +0530

    update list item with new image url and improved UI

--- lib/ui/widgets/list_item_pokemon.dart
+++ lib/ui/widgets/list_item_pokemon.dart
@@ -1,3 +1,4 @@
+import 'package:Pokydx/ui/widgets/types_widget.dart';
 import 'package:flutter/material.dart';
 
 import '../../data/pokemon.dart';
@@ -6,46 +7,84 @@ class ListItemPokemon extends StatelessWidget {
   final Pokemon pokemon;
   final Function onTapPokemon;
   final int id;
+  final String name;
+  final int speciesId;
 
   static const String BASE_IMG_URL =
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+      "https://cdn.jsdelivr.net/gh/MohammedAkhil/sprites/sprites/pokemon/";
 
-  ListItemPokemon({Key key, this.onTapPokemon, this.pokemon, this.id})
+  ListItemPokemon(
+      {Key key,
+      this.onTapPokemon,
+      this.pokemon,
+      this.id,
+      this.name,
+      this.speciesId})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      contentPadding: EdgeInsets.all(0),
+    return InkWell(
       onTap: () {
         onTapPokemon(pokemon);
       },
-      leading: pokeImageWidget(),
-      title: Text(
-        pokemon.name,
-        style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
-      ),
-      subtitle:
-          Text(getPokeNumber(), style: TextStyle(color: Colors.black38, fontSize: 16)),
-      trailing: Text(
-        getGeneration(),
-        style: TextStyle(color: Colors.black54),
+      child: Card(
+        margin: EdgeInsets.fromLTRB(12, 4, 12, 4),
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(8))),
+        elevation: 0,
+        child: Stack(
+          children: <Widget>[
+            legendaryLogo(),
+            Container(
+              margin: EdgeInsets.all(8),
+              child: Row(
+                children: <Widget>[
+                  image(),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        pokemon.name,
+                        style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      pokedexNumber(context),
+                    ],
+                  ),
+                  Spacer(),
+                  types(),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 
-  Widget pokeImageWidget() {
+  Widget pokedexNumber(BuildContext context) {
+    return Text(formatPokedexNumber(),
+        style: Theme.of(context).textTheme.body1);
+  }
+
+  Widget image() {
     return Container(
-      width: 100,
-      height: 100,
+      width: 60,
+      height: 60,
       child: FadeInImage.assetNetwork(
-        image: BASE_IMG_URL + (id).toString() + '.png',
+        image: getImageUrl(id),
         placeholder: 'assets/images/pokeLogo.png',
       ),
     );
   }
 
-  String getPokeNumber() {
+  String formatPokedexNumber() {
     int preZeroCount = 3 - (id).toString().length;
     String preZeroes;
     switch (preZeroCount) {
@@ -61,11 +100,42 @@ class ListItemPokemon extends StatelessWidget {
     return "#$preZeroes$id";
   }
 
-  String getGeneration() {
-    if (id > 151) {
-      return '2nd gen';
-    } else {
-      return '1st gen';
+  Widget types() {
+    return TypesWidget(
+      types: pokemon.type,
+      isSmall: true,
+    );
+  }
+
+  Widget legendaryLogo() {
+    return pokemon.isLegendary
+        ? Positioned(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                  width: 16,
+                  child: Image.asset('assets/icons/ic-legendary.png')),
+            ),
+            bottom: 0,
+            right: 0,
+          )
+        : Container();
+  }
+
+  String getImageUrl(int id) {
+    if (id <= 809) {
+      return BASE_IMG_URL + id.toString() + '.png';
     }
+    String baseUrl =
+        "https://cdn.jsdelivr.net/gh/MohammedAkhil/sprites/sprites/pokemon/";
+    String imageId = id <= 10090
+        ? id.toString()
+        : speciesId.toString() + getSubSpeciesName(name);
+    return baseUrl + imageId + '.png';
+  }
+
+  String getSubSpeciesName(String fullName) {
+    return fullName.substring(
+        fullName.indexOf(RegExp(r'-[\w]{2,}').stringMatch(fullName).toString()));
   }
 }
